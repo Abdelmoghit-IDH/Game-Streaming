65876ee9e08fbe1c4693db5474db07e2118923d3
Abdelmoghit <idhsaine.abdelmoghit@inemail.ine.inpt.ma>
2022-06-05 18:31:38 +0100

    add profile

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -8,36 +8,41 @@ import Results from "./components/Results/Results";
 import Error from "./components/Error/Error";
 import Login from "./pages/signin";
 import SignUp from "./pages/signup";
-// import Player from "./components/TheProfile/profile";
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "./theme";
+import Cover from "./components/Profile/Cover";
+import Main from "./components/Profile/Main";
 import Check from "./components/player/Check";
-import {
-  Route,
-  Switch,
-  useLocation,
-} from "react-router-dom";
+import { Route, Switch, useLocation } from "react-router-dom";
 
 function App() {
   const location = useLocation().pathname;
   const toOverride = ["sign-in", "sign-up", "profile"];
 
   return (
-      <div className="App">
-        {/* Fixed */}
-        {!new RegExp(toOverride.join("|")).test(location) && <Header />}
-        {!new RegExp(toOverride.join("|")).test(location) && <Sidebar />}
-        {/* \ Fixed */}
-        <Switch>
-          <Route exact path="/" component={Games} />
-          <Route exact path="/top-streams" component={TopStreams} />
-          <Route exact path="/stream" component={Check} />
-          <Route exact path="/live/:slug" component={Check} />
-          <Route exact path="/game/:slug" component={GameStreams} />
-          <Route exact path="/results/:slug" component={Results} />
-          <Route exact path="/results/" component={Error} />
-          <Route exact path="/sign-in/" component={Login} />
-          <Route exact path="/sign-up/" component={SignUp} />
-        </Switch>
-      </div>
+    <div className="App">
+      {/* Fixed */}
+      {!new RegExp(toOverride.join("|")).test(location) && <Header />}
+      {!new RegExp(toOverride.join("|")).test(location) && <Sidebar />}
+      {/* \ Fixed */}
+      <Switch>
+        <Route exact path="/" component={Games} />
+        <Route exact path="/top-streams" component={TopStreams} />
+        <Route exact path="/stream" component={Check} />
+        <Route exact path="/live/:slug" component={Check} />
+        <Route exact path="/game/:slug" component={GameStreams} />
+        <Route exact path="/results/:slug" component={Results} />
+        <Route exact path="/results/" component={Error} />
+        <Route exact path="/sign-in/" component={Login} />
+        <Route exact path="/sign-up/" component={SignUp} />
+        <ChakraProvider theme={theme}>
+          <Route path="/profile" exact>
+            <Cover />
+            <Main />
+          </Route>
+        </ChakraProvider>
+      </Switch>
+    </div>
   );
 }
 
